7db9229eef84b280072f9cf8731f1cd40d5e6cfa
bratiSlava92 <2405bratiSlava@gmail.com>
2022-02-05 23:01:40 +0300

    Ex6, GET /long_redirect

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+# Ex.4
+# response = requests.get("https://playground.learnqa.ru/api/get_text")
+# print(response.text)
 
 
-print("Hello from Alina Borlakova!")
+# Ex.3
+# print("Hello from Alina Borlakova!")
+
+
+# Ex.6 узнать сколько редиректов происходит от изначальной точки назначения до итоговой. И какой URL итоговый.
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+response.history
+print(f"Количество редиректов =", len(response.history))
+print(f"Конечный URL:", response.url)
