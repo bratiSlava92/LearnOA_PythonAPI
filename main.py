280e358c713c4c9b5c502c0f0946154b72f46524
bratiSlava92 <2405bratiSlava@gmail.com>
2022-02-09 22:55:08 +0300

    Ex.9 Find password

--- main.py
+++ main.py
@@ -50,20 +50,42 @@ import time
 #     print("Method DELETE", response.status_code, response.text)
 #     print()
 
+
 # Ex8: Токены
-job_url = "https://playground.learnqa.ru/ajax/api/longtime_job"
-response = requests.get(job_url)
-token = response.json()["token"]
-seconds = response.json()["seconds"]
-res1 = requests.get(job_url, params={"token": token})
-if "status" in res1.json():
-    if res1.json()["status"] == "Job is NOT ready":
-        time.sleep(seconds)
-        res2 = requests.get(job_url, params={"token": token})
-        print(res2.text)
-        assert res2.json()["result"] is not None, "Missing parameter result"
-        assert res2.json()["status"] == "Job is ready", "Task Status is INCORRECT"
-    else:
-        print("Status is INCORRECT:", res1.text)
-else:
-    print(res1.text)
+# job_url = "https://playground.learnqa.ru/ajax/api/longtime_job"
+# response = requests.get(job_url)
+# token = response.json()["token"]
+# seconds = response.json()["seconds"]
+# res1 = requests.get(job_url, params={"token": token})
+# if "status" in res1.json():
+#     if res1.json()["status"] == "Job is NOT ready":
+#         time.sleep(seconds)
+#         res2 = requests.get(job_url, params={"token": token})
+#         print(res2.text)
+#         assert res2.json()["result"] is not None, "Missing parameter result"
+#         assert res2.json()["status"] == "Job is ready", "Task Status is INCORRECT"
+#     else:
+#         print("Status is INCORRECT:", res1.text)
+# else:
+#     print(res1.text)
+
+
+# Ex9: Подбор пароля
+# Top 25 most common passwords by year according to SplashData
+top_25_passwords = ["000000", "111111", "121212", "123123", "1234", "12345", "123456", "1234567", "12345678", "123456789",
+                     "1234567890", "555555", "654321", "666666", "696969", "7777777", "888888", "123qwe", "1qaz2wsx",
+                     "1q2w3e4r", "aa123456", "abc123", "access", "admin", "adobe123[a]", "ashley", "azerty", "access",
+                     "bailey", "baseball", "batman", "charlie", "donald", "dragon", "flower", "football", "Football",
+                     "freedom", "hello", "hottie", "iloveyou", "jesus", "letmein", "login", "lovely", "loveme", "master",
+                     "michael", "monkey", "mustang", "ninja",  "password", "passw0rd", "password1", "photoshop[a]",
+                     "princess", "qazwsx", "qwerty", "qwertyuiop", "qwerty", "qwerty123", "shadow", "solo", "starwars",
+                     "sunshine", "superman", "trustno1", "whatever", "welcome", "zaq1zaq1", "!@#$%^&*"]
+for i in top_25_passwords:
+    payload = {"login": "super_admin", "password": i}
+    response = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework", data=payload)
+    cookie_value = response.cookies.get("auth_cookie")
+    check_res = requests.post("https://playground.learnqa.ru/ajax/api/check_auth_cookie", cookies={"auth_cookie": cookie_value})
+    if check_res.text != "You are NOT authorized":
+        print("Correct password:", i)
+        print(check_res.text)
+
